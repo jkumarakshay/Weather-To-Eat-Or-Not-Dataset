638bd1a614867e32782f1141b41e6aeed640eb22
oustella <oustella@users.noreply.github.com>
2018-12-07 16:42:47 -0500

    update

--- API_request.py
+++ API_request.py
@@ -1,7 +1,23 @@
+## Import dependencies
+## Install as needed
+import requests, time, json, datetime
+from urllib.parse import quote
 
-# Specify user file path for saved files
+## Tune parameters where needed
+## This is the only section user input is required
+    # Yelp Fusion API Key
+user_key = '5NczmeloT_5Qc8niaCRJfZRT5P83RIQces_05BZgQqzYZ8uO6LPDBNHtl59auUzg5ukvsdSFWHIJnwesgrp7NfSA-4Xvo1juZSH0IJsUK1SlNk7KhZEVet_-kwrAW3Yx'
+    # OpenWeatherMap API Key
+user_owmKey = "ffc55dc8f3dd47ff5e138b730603d46a"
+business_type = 'restaurant'
+sort_criteria = 'rating'
+user_location = 'Philadelphia, PA'
+    # Specify user file path for saved files
 filepath = '/Users/ouliang/Google Drive/DRAGON/Data Preprocessing/Group Project/'
 
+#========================================================#
+####### No user input is required from here and on########
+#========================================================#
 
 """
 ###########################
@@ -9,15 +25,8 @@ filepath = '/Users/ouliang/Google Drive/DRAGON/Data Preprocessing/Group Project/
 ###########################
 """
 
-import requests
-
-# Create request header for authorization
-user_key = '5NczmeloT_5Qc8niaCRJfZRT5P83RIQces_05BZgQqzYZ8uO6LPDBNHtl59auUzg5ukvsdSFWHIJnwesgrp7NfSA-4Xvo1juZSH0IJsUK1SlNk7KhZEVet_-kwrAW3Yx'
-
-from urllib.parse import quote
-
+## Construct urls
 results = [] # results from daily queries are appended into a list for that day
-
 def request(host, path, api_key, url_params):
     """
         Define a request function that constructs the url based on input parameters and return the results
@@ -29,6 +38,7 @@ def request(host, path, api_key, url_params):
     response = requests.request('GET', url, headers=header, params=url_params)
     return results.append(response.json())
 
+## Construct search function
 def search(api_key, term, location, limit, sort_by, offset):
     """
         Define a search function for inputting paremeters
@@ -48,12 +58,6 @@ API_HOST = 'http://api.yelp.com'
 SEARCH_PATH = '/v3/businesses/search'
 BUSINESS_PATH = '/v3/businesses/'
 
-## Tune parameters where needed
-business_type = 'restaurant'
-sort_criteria = 'rating'
-user_location = 'Philadelphia, PA'
-
-import time
 SEARCH_LIMIT = 50 # This is the max number of business results to return.
 DAILY_CALLS = 20
 
@@ -69,8 +73,6 @@ def calls(location, api_key):
         time.sleep(1) # Wait for a second between calls
 
 # Create daily files in order to preserve previous results in case of errors being appended to the same file
-import json
-import datetime
 
 today = datetime.date.today().strftime("%m%d%y")
 
@@ -85,9 +87,7 @@ def YelpJob():
 ##############################
 """
 
-#OpenWeatherMap API key
-user_owmKey = "ffc55dc8f3dd47ff5e138b730603d46a"
-endpoint="http://api.openweathermap.org/data/2.5/weather?q=Philadelphia,us&APPID="+user_owmKey
+endpoint=f"http://api.openweathermap.org/data/2.5/weather?q={user_location}&APPID={user_owmKey}""
 
 def weatherJob():
     """ Acquire today's weather """
--- README.md
+++ README.md
@@ -1,22 +1,25 @@
 # Weather-To-Eat-Or-Not-Dataset
 
-## Sections
-
-### Introduction - Abeer
+## Introduction - Abeer
 Scope, motivation and purpose.
-### Data Dictionary - Devanshi
+
+
+
+## Data Dictionary - Devanshi
 Explanation of each attribute in Yelp and OpenWeatherMap and why certain attributes were eliminated in dataset.
 
 
-### Working with the APIs - Stella
-This program allows users to customize location and business type when querying the Yelp Fusion API.
-
-## An introduction on Yelp Fusion APIs
-
-## An introduction on OpenWeatherMap APIs
+## Working with the APIs - Stella
+This program allows users to obtain restaurant review and weather information based on customized location and business type.
 
 
-### Data aggregation/processing - Akshay
+### An introduction on Yelp Fusion APIs
+
+
+### An introduction on OpenWeatherMap APIs
+
+
+## Data aggregation/processing - Akshay
 Explanation of how JSON files were manipulated and CSV files are created.
-### Challenges/Discussions/Future Work - All
+## Challenges/Discussions/Future Work - All
 Everyone gives her/his two cents. This WILL take a while.
